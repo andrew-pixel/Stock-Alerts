3eb0b247830ebff501fd428e0cf2b7335e1700d9
Andrew Yoo <yooandy43@gmail.com>
2025-04-08 15:12:13 -0500

    update yaml

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use reqwest::{Client};
-use serde_json::{Value, json};
-use std::env;
-use serde::{ Deserialize, Serialize}; 
 use dotenvy::dotenv;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use std::env;
 
+use lambda_runtime::{service_fn, Context, Error, LambdaEvent};
 use yahoo_finance_api as yahoo;
-use lambda_runtime::{Error, service_fn, LambdaEvent, Context};
 
-
-#[derive(Serialize , Debug, Deserialize)]
+#[derive(Serialize, Debug, Deserialize)]
 pub struct StockPrice {
     pub name: String,
     pub lastprice: f64,
@@ -22,12 +21,11 @@ struct Response {
 
 pub async fn function_handler(event: LambdaEvent<Value>) -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    
+
     let supabase_url = env::var("URL").expect("Missing SUPABASE URL in .env");
     let supabase_key = env::var("APIKEY").expect("Missing SUPABASE KEY in .env");
 
     let client = Client::new();
-    
 
     let response = client
         .get(format!("{}/rest/v1/stocks", supabase_url))
@@ -36,7 +34,7 @@ pub async fn function_handler(event: LambdaEvent<Value>) -> Result<(), Box<dyn s
         .header("Content-Type", "application/json")
         .send()
         .await?;
-    
+
     let response_text = response.text().await?;
     let stock_prices: Vec<StockPrice> = match serde_json::from_str(&response_text) {
         Ok(data) => data,
@@ -48,7 +46,11 @@ pub async fn function_handler(event: LambdaEvent<Value>) -> Result<(), Box<dyn s
 
     let provider = yahoo::YahooConnector::new()?;
 
-    let event_type = event.payload.get("event_type").and_then(|v| v.as_str()).unwrap_or("");
+    let event_type = event
+        .payload
+        .get("event_type")
+        .and_then(|v| v.as_str())
+        .unwrap_or("");
 
     for stock in &stock_prices {
         println!("Stock: {}, Price: {}", stock.name, stock.lastprice);
@@ -58,16 +60,19 @@ pub async fn function_handler(event: LambdaEvent<Value>) -> Result<(), Box<dyn s
                 if price_diff > 0.04 {
                     let positive_negative = if quote.close < stock.lastprice { -1 } else { 1 };
                     updateDatabase(&supabase_url, &supabase_key, quote.close, &stock.name).await?;
-                    send_pushbullet_notification(&stock.name, positive_negative, quote.close, price_diff*100.0 ).await?;
-                    
-                }
-                else if event_type == "close" {
+                    send_pushbullet_notification(
+                        &stock.name,
+                        positive_negative,
+                        quote.close,
+                        price_diff * 100.0,
+                    )
+                    .await?;
+                } else if event_type == "close" {
                     updateDatabase(&supabase_url, &supabase_key, quote.close, &stock.name).await?;
                 }
             }
         }
     }
-    
 
     Ok(())
 }
@@ -81,47 +86,61 @@ async fn main() -> Result<(), Error> {
     //let event = serde_json::json!({ "test": "data" }); // Simulated event payload
     //let ctx = lambda_runtime::Context::default(); // Dummy context
 
-   // let result = function_handler(event, ctx).await?;
+    // let result = function_handler(event, ctx).await?;
     //println!("Function Result: {}", result);
 
     Ok(())
 }
 
-async fn updateDatabase(url: &str, key: &str, price: f64, name : &str ) ->  Result<(), Box<dyn std::error::Error>>{
+async fn updateDatabase(
+    url: &str,
+    key: &str,
+    price: f64,
+    name: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
     let priceR = (price * 100.0).round() / 100.0;
     let payload = json!({
-        "lastprice": priceR 
+        "lastprice": priceR
     });
 
     let update = format!("{}/rest/v1/stocks?name=eq.{}", url, name);
-    let response = client.patch(&update)
-    .header("apikey", key)
-    .header("Authorization", format!("Bearer {}", key))
-    .header("Content-Type", "application/json")
-    .json(&payload)
-    .send()
-    .await?;
+    let response = client
+        .patch(&update)
+        .header("apikey", key)
+        .header("Authorization", format!("Bearer {}", key))
+        .header("Content-Type", "application/json")
+        .json(&payload)
+        .send()
+        .await?;
 
     if response.status().is_success() {
         println!("Updated stock {} with new price: {}", name, price);
     } else {
-        println!("Failed to update stock {}: {:?}", name, response.text().await?);
+        println!(
+            "Failed to update stock {}: {:?}",
+            name,
+            response.text().await?
+        );
     }
     Ok(())
 }
-pub async fn send_pushbullet_notification(title: &str, positive: i32, price: f64, percent_change: f64) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn send_pushbullet_notification(
+    title: &str,
+    positive: i32,
+    price: f64,
+    percent_change: f64,
+) -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
     let access_token = env::var("PUSHAPIKEY").expect("Missing PUSHAPIKEY in .env");
-    
+
     let title2 = if positive == 1 {
         format!(" {} +{:.2}%", title, percent_change)
     } else {
         format!(" {} -{:.2}%", title, percent_change)
     };
     let body = format!("Price: ${:.2}", price);
-    
-    
+
     let payload = json!({
         "type": "note",
         "title": title2,
@@ -150,16 +169,18 @@ pub mod test {
     pub use super::*;
 }
 pub mod handlers {
+    pub use super::function_handler;
     pub use super::send_pushbullet_notification;
     pub use super::testable_function_handler;
-    pub use super::function_handler;
 }
-pub async fn testable_function_handler(event: LambdaEvent<Value>, test_stocks: Option<Vec<StockPrice>>)
- -> Result<TestResults, Box<dyn std::error::Error>> { // separate clone function handler to not break aws lambda
-    
-    
+pub async fn testable_function_handler(
+    event: LambdaEvent<Value>,
+    test_stocks: Option<Vec<StockPrice>>,
+) -> Result<TestResults, Box<dyn std::error::Error>> {
+    // separate clone function handler to not break aws lambda
+
     dotenv().ok();
-    
+
     let mut stocks_result = if let Some(test_stocks) = test_stocks {
         test_stocks
     } else {
@@ -174,37 +195,37 @@ pub async fn testable_function_handler(event: LambdaEvent<Value>, test_stocks: O
             .header("Content-Type", "application/json")
             .send()
             .await?;
-        
+
         let response_text = response.text().await?;
         serde_json::from_str(&response_text)?
     };
-    
+
     let provider = yahoo::YahooConnector::new()?;
 
-    let event_type = event.payload.get("event_type").and_then(|v| v.as_str()).unwrap_or("");
-    let mut results = TestResults {
-        stocks: Vec::new(),
-    };
+    let event_type = event
+        .payload
+        .get("event_type")
+        .and_then(|v| v.as_str())
+        .unwrap_or("");
+    let mut results = TestResults { stocks: Vec::new() };
     for stock in &stocks_result {
         println!("Stock: {}, Price: {}", stock.name, stock.lastprice);
-        if stock.name == "TEST1"{
+        if stock.name == "TEST1" {
             let price_diff = ((100.0 - stock.lastprice) / stock.lastprice).abs();
-                let mut action = "none";
-                if price_diff > 0.04 {
-                    action = "significant price %";
-                }
-                else if event_type == "close" {
-                    action = "close"
-                }
-                results.stocks.push(StockR{
-                    name: stock.name.clone(),
-                    previousPrice: stock.lastprice,
-                    currentPrice: 100.0,
-                    percent: price_diff*100.0,
-                    action: action.to_string(),
-                });
-        }
-        else if let Ok(response) = provider.get_latest_quotes(&stock.name, "1d").await {
+            let mut action = "none";
+            if price_diff > 0.04 {
+                action = "significant price %";
+            } else if event_type == "close" {
+                action = "close"
+            }
+            results.stocks.push(StockR {
+                name: stock.name.clone(),
+                previousPrice: stock.lastprice,
+                currentPrice: 100.0,
+                percent: price_diff * 100.0,
+                action: action.to_string(),
+            });
+        } else if let Ok(response) = provider.get_latest_quotes(&stock.name, "1d").await {
             if let Ok(quote) = response.last_quote() {
                 let price_diff = ((quote.close - stock.lastprice) / stock.lastprice).abs();
                 let mut action = "none";
@@ -212,23 +233,20 @@ pub async fn testable_function_handler(event: LambdaEvent<Value>, test_stocks: O
                     let positive_negative = if quote.close < stock.lastprice { -1 } else { 1 };
                     //updateDatabase(&supabase_url, &supabase_key, quote.close, &stock.name).await?;
                     action = "significant price %";
-                }
-                else if event_type == "close" {
+                } else if event_type == "close" {
                     //updateDatabase(&supabase_url, &supabase_key, quote.close, &stock.name).await?;
                     action = "close"
                 }
-                results.stocks.push(StockR{
+                results.stocks.push(StockR {
                     name: stock.name.clone(),
                     previousPrice: stock.lastprice,
                     currentPrice: quote.close,
-                    percent: price_diff*100.0,
+                    percent: price_diff * 100.0,
                     action: action.to_string(),
                 });
-
             }
         }
     }
-    
 
     Ok(results)
 }
@@ -238,10 +256,10 @@ pub struct TestResults {
     pub stocks: Vec<StockR>,
 }
 #[derive(Debug)]
-pub struct StockR{
+pub struct StockR {
     pub name: String,
     pub previousPrice: f64,
     pub currentPrice: f64,
-    pub percent:f64,
-    pub action: String, 
+    pub percent: f64,
+    pub action: String,
 }
--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,10 @@
-use stockalerts::handlers::{send_pushbullet_notification, testable_function_handler, function_handler};
+use stockalerts::handlers::{
+    function_handler, send_pushbullet_notification, testable_function_handler,
+};
 use stockalerts::{StockPrice, TestResults};
 
 use lambda_runtime::{Context, LambdaEvent};
-use serde_json::{json,Value};
+use serde_json::{json, Value};
 
 #[cfg(test)]
 mod tests {
@@ -15,63 +17,52 @@ mod tests {
     }
     #[tokio::test]
     async fn fulltest() {
-        let event = LambdaEvent::new(
-            json!({"event_type": ""}),
-            Context::default()
-        );
+        let event = LambdaEvent::new(json!({"event_type": ""}), Context::default());
         let results = function_handler(event).await;
-        
+
         assert!(results.is_ok());
     }
     #[tokio::test]
     async fn test_close_event() {
-        let event = LambdaEvent::new(
-            json!({"event_type": "close"}),
-            Context::default()
-        );
-        
+        let event = LambdaEvent::new(json!({"event_type": "close"}), Context::default());
+
         let results = testable_function_handler(event, None).await.unwrap();
-        
+
         for stock_result in &results.stocks {
-            
             assert_eq!(stock_result.action, "close");
-            
         }
     }
     #[tokio::test]
     async fn test_price_diff() {
         let test_stocks = vec![
-        StockPrice {
-            name: "TEST1".to_string(),
-            lastprice: 101.0 // none
-        },
-        StockPrice {
-            name: "TEST1".to_string(),
-            lastprice: 105.0, // 5%+
-        },
-        StockPrice {
-            name: "TEST1".to_string(),
-            lastprice: 50.0, // -5%
-        },
+            StockPrice {
+                name: "TEST1".to_string(),
+                lastprice: 101.0, // none
+            },
+            StockPrice {
+                name: "TEST1".to_string(),
+                lastprice: 105.0, // 5%+
+            },
+            StockPrice {
+                name: "TEST1".to_string(),
+                lastprice: 50.0, // -5%
+            },
+        ];
+        let event = LambdaEvent::new(json!({"event_type": ""}), Context::default());
 
-    ];
-        let event = LambdaEvent::new(
-            json!({"event_type": ""}),
-            Context::default()
-        );
-        
-        let results = testable_function_handler(event, Some(test_stocks)).await.unwrap();
-        
+        let results = testable_function_handler(event, Some(test_stocks))
+            .await
+            .unwrap();
+
         println!("{:#?}", results);
-    
+
         // Example assertions
         for stock_result in &results.stocks {
             if stock_result.percent > 4.0 {
                 assert_eq!(stock_result.action, "significant price %");
-            } 
-            else if stock_result.percent < 4.0 && stock_result.action == "none"{
+            } else if stock_result.percent < 4.0 && stock_result.action == "none" {
                 assert_eq!(stock_result.action, "none");
-            } 
+            }
         }
     }
 }
